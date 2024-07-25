a82ac87369dcba3a60b601d03075b0935f941c68
Utkarsh-Weber21 <120705388+Utkarsh-Weber21@users.noreply.github.com>
2024-07-25 14:22:36 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://utkarsh-blogging-app.netlify.app/'
+    'https://utkarsh-blogging-app.netlify.app'
 
 ];
 
